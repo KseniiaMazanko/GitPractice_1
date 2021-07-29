6224368d32bb23c67f4b6288c8bef369854ab10c
KseniiaMazanko <ksmazanko@gmail.com>
2021-07-29 14:30:30 -0400

    New commit

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -5,6 +5,7 @@ public class ListPractice {
     public static void main(String[] args) {
 
         System.out.println("New branch created");
+        System.out.println("Making a new class");
 
     }
 
