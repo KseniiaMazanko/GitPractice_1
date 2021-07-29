a29f3a45eba5d9f0bd494058021a45b222c3a51d
KseniiaMazanko <ksmazanko@gmail.com>
2021-07-29 14:31:19 -0400

    New ArrayList created

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,11 +1,16 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ListPractice {
 
     public static void main(String[] args) {
 
         System.out.println("New branch created");
         System.out.println("Making a new class");
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("Hello", "Just checking"));
 
     }
 
