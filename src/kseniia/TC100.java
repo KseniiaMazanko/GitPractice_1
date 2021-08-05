1d39f01574c973c07e3de5e10202b5853080f7a9
KseniiaMazanko <ksmazanko@gmail.com>
2021-08-05 15:43:39 -0400

    print out statement

--- src/kseniia/TC100.java
+++ src/kseniia/TC100.java
@@ -1,10 +1,12 @@
 package kseniia;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class TC100 {
 
     //here's the initial commit
 
     public static void main(String[] args) {
-
+        System.out.println("Hello");
     }
 }
